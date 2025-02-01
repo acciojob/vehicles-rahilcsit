f4e420379b1f5dc746a1563b6c04ebf663d9160f
rahilcsit <rahilcsit@gmail.com>
2025-02-02 01:13:49 +0530

    solved assignment

---
+++ src/main/java/com/driver/WaterVehicleImplementation.java
@@ -0,0 +1,9 @@
+package com.driver;
+
+public class WaterVehicleImplementation {
+//    Boat myBoat = new Boat("Yacht", 10);
+//
+//    // Test the Boat methods
+//        System.out.println("Boat Name: " + myBoat.getVehicleName());
+//        System.out.println("Boat Capacity: " + myBoat.getVehicleCapacity());
+}
